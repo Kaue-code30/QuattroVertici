ee798a692641a1131cd50950e528ce5ab52c0689
Kauê Lima <kaue.slima000@gmail.com>
2024-11-22 11:33:14 -0300

    footer

--- app/src/Home/Components/Contact/index.tsx
+++ app/src/Home/Components/Contact/index.tsx
@@ -8,7 +8,7 @@ function Contact() {
     return (
         <section className="w-full h-auto flex items-center justify-center text-white bg-black pt-[100px] pb-[50px]">
             <div className="max-w-[1000px] h-full flex flex-col  items-start gap-3 justify-start w-4/5 lg:w-full">
-                <div className="w-full h-full flex-col gap-4 flex ">
+                <div className="w-full h-full flex-col  gap-4 flex ">
                     <h1 className="text-4xl leading ">
                         Lorem ipsum dolor sit amet consectetur.
                     </h1>
@@ -19,12 +19,12 @@ function Contact() {
 
                     </div>
                 </div>
-                <div className="h-auto flex justify-between py-14  w-full">
-                    <div className="w-auto flex flex-col gap-3 h-full">
+                <div className="h-auto flex lg:flex-row flex-col justify-between py-14  w-full">
+                    <div className="w-auto flex flex-col lg:pb-0 pb-10 gap-3 h-full">
                         <h1 className="text-white cursor-default pb-2 font-medium text-xl lg:text-3xl">
                             Quattro Vertici
                         </h1>
-                        <Link href={"mailto:email@quattrovertici@gmail.com"} className="flex font-light text-sm items-center justify-center gap-1">
+                        <Link href={"mailto:email@quattrovertici@gmail.com"} className="flex font-light text-sm items-center justify-start gap-1">
                             <IoMailOutline fontSize={22} />
                             email@quattrovertici@gmail.com
                         </Link>
@@ -33,11 +33,11 @@ function Contact() {
                             Outra informação
                         </Link>
                     </div>
-                    <div className=" flex gap-20">
+                    <div className=" flex lg:flex-row flex-col gap-10 lg:gap-20">
 
 
                         <div className="w-auto h-full">
-                            <h2 className="font-light text-2xl ">
+                            <h2 className="font-light text-xl lg:text-2xl ">
                                 Links Úteis:
                             </h2>
                             <ul className="pt-3 flex flex-col gap-2">
@@ -54,7 +54,7 @@ function Contact() {
                             </ul>
                         </div>
                         <div className="w-auto h-full">
-                            <h2 className="font-light text-2xl ">
+                            <h2 className="font-light text-xl lg:text-2xl ">
                                 Empresas:
                             </h2>
                             <ul className="pt-3 flex flex-col gap-2">
